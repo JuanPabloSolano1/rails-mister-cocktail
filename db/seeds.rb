1182bd182ef3fee56c50199302bc517c9f957698
juan solano <juanpablosolanosuarez@hotmail.com>
2019-08-16 08:19:27 -0700

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,25 @@ Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 
-Cocktail.create(name: "Daiquiri")
-Cocktail.create(name: "Tequila")
+
+puts "I am now creating cocktail"
+url1 = "http://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2013/11/mojito-cocktails.jpg?itok=7ZS6egg_"
+url2 = "http://www.monstercocktails.com/image/279/645"
+url3 = "https://705628.smushcdn.com/1130679/wp-content/uploads/2019/05/20rustic-cocktail.jpg?lossy=1&strip=1&webp=1"
+url4 = "https://www.thespruceeats.com/thmb/VUIEfKs1iqGuO9FHqY4eYzPg6Ks=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/NavyGrogCocktail-565092779-577437e33df78cb62c3c37fd.jpg"
+url5 = "https://cocktailcollective.com.au/wp-content/uploads/2018/10/Surprise-Sour-565x565.jpg"
+cocktail1 = Cocktail.new(name: "Mochito")
+cocktail1.remote_photo_url = url1
+cocktail1.save
+cocktail2 = Cocktail.new(name: "Caipichina")
+cocktail2.remote_photo_url = url2
+cocktail2.save
+cocktail3 = Cocktail.new(name: "dark rise")
+cocktail3.remote_photo_url = url3
+cocktail3.save
+cocktail4 = Cocktail.new(name: "grog")
+cocktail4.remote_photo_url = url4
+cocktail4.save
+cocktail5 = Cocktail.new(name: "bad surprise")
+cocktail5.remote_photo_url = url5
+cocktail5.save
